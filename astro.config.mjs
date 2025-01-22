c78e57a6dba0f9b0a3762d307dbf7d7bceae7859
RelativeSure <mail@rasmusj.dk>
2025-01-23 00:19:27 +0100

    feat: add robots.txt integration and update wrangler configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,55 +6,55 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rasmusj.dk',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    starlight({
-      plugins: [starlightImageZoom()],
-      title: 'Rasmus Brøgger Jørgensen',
-      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
-      lastUpdated: true,
-      social: {
-        github: 'https://github.com/RelativeSure',
-        mastodon: 'https://infosec.exchange/@relativesure',
-        'x.com': 'https://x.com/RelativeSure',
-        linkedin: 'https://www.linkedin.com/in/rasmusbroeggerjoergensen/'
-      },
-      sidebar: [{
-        label: 'IT Security',
-        autogenerate: {
-          directory: 'it-security'
-        }
-      }, {
-        label: 'Kubernetes',
-        autogenerate: {
-          directory: 'kubernetes'
-        }
-      }, {
-        label: 'Linux',
-        autogenerate: {
-          directory: 'linux'
-        }
-      }, {
-        label: 'Windows',
-        autogenerate: {
-          directory: 'windows'
-        }
-      }, {
-        label: 'Bookmarks',
-        autogenerate: {
-          directory: 'bookmarks'
-        }
-      }, {
-        label: 'Projects',
-        autogenerate: {
-          directory: 'projects'
-        }
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), starlight({
+    plugins: [starlightImageZoom()],
+    title: 'Rasmus Brøgger Jørgensen',
+    tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+    lastUpdated: true,
+    social: {
+      github: 'https://github.com/RelativeSure',
+      mastodon: 'https://infosec.exchange/@relativesure',
+      'x.com': 'https://x.com/RelativeSure',
+      linkedin: 'https://www.linkedin.com/in/rasmusbroeggerjoergensen/'
+    },
+    sidebar: [{
+      label: 'IT Security',
+      autogenerate: {
+        directory: 'it-security'
       }
-    ]
-  }), sitemap(), mdx(), react(), tailwind()]
+    }, {
+      label: 'Kubernetes',
+      autogenerate: {
+        directory: 'kubernetes'
+      }
+    }, {
+      label: 'Linux',
+      autogenerate: {
+        directory: 'linux'
+      }
+    }, {
+      label: 'Windows',
+      autogenerate: {
+        directory: 'windows'
+      }
+    }, {
+      label: 'Bookmarks',
+      autogenerate: {
+        directory: 'bookmarks'
+      }
+    }, {
+      label: 'Projects',
+      autogenerate: {
+        directory: 'projects'
+      }
+    }
+  ]
+}), sitemap(), mdx(), react(), tailwind(), robotsTxt()]
 });
