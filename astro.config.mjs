01f1abd118e040ee8ae1bf35b687d2896b4fe8d7
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2025-01-22 11:23:02 +0100

    Update site and add shadcn (#547)
    * Update site and add shadcn
    
    * remove uneeded SSR
    
    * Update .mega-linter.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,67 +2,59 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightImageZoom from 'starlight-image-zoom';
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rasmusj.dk',
-  adapter: cloudflare({
-     imageService: 'passthrough',
-     platformProxy: {
-      enabled: true,
-    },
-  }),
-  output: 'server',
-  vite: {
-    ssr: {
-      external: ['node:buffer', 'node:path', 'node:url'],
-    },
-  },
-  integrations: [starlight({
-    plugins: [starlightImageZoom()],
-    title: 'Rasmus Brøgger Jørgensen',
-    lastUpdated: true,
-    social: {
-      github: 'https://github.com/RelativeSure',
-      mastodon: 'https://infosec.exchange/@relativesure',
-      twitter: 'https://twitter.com/RelativeSure',
-      linkedin: 'https://www.linkedin.com/in/rasmusbroeggerjoergensen/'
-    },
-    sidebar: [{
-      label: 'IT Security',
-      autogenerate: {
-        directory: 'it-security'
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    starlight({
+      plugins: [starlightImageZoom()],
+      title: 'Rasmus Brøgger Jørgensen',
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+      lastUpdated: true,
+      social: {
+        github: 'https://github.com/RelativeSure',
+        mastodon: 'https://infosec.exchange/@relativesure',
+        'x.com': 'https://x.com/RelativeSure',
+        linkedin: 'https://www.linkedin.com/in/rasmusbroeggerjoergensen/'
+      },
+      sidebar: [{
+        label: 'IT Security',
+        autogenerate: {
+          directory: 'it-security'
+        }
+      }, {
+        label: 'Kubernetes',
+        autogenerate: {
+          directory: 'kubernetes'
+        }
+      }, {
+        label: 'Linux',
+        autogenerate: {
+          directory: 'linux'
+        }
+      }, {
+        label: 'Windows',
+        autogenerate: {
+          directory: 'windows'
+        }
+      }, {
+        label: 'Bookmarks',
+        autogenerate: {
+          directory: 'bookmarks'
+        }
+      }, {
+        label: 'Projects',
+        autogenerate: {
+          directory: 'projects'
+        }
       }
-    }, {
-      label: 'Kubernetes',
-      autogenerate: {
-        directory: 'kubernetes'
-      }
-    }, {
-      label: 'Linux',
-      autogenerate: {
-        directory: 'linux'
-      }
-    }, {
-      label: 'Windows',
-      autogenerate: {
-        directory: 'windows'
-      }
-    }, {
-      label: 'Bookmarks',
-      autogenerate: {
-        directory: 'bookmarks'
-      }
-    }, {
-      label: 'Projects',
-      autogenerate: {
-        directory: 'projects'
-      }
-    }
-  ]
-  }),
-  sitemap(),
-  mdx()]
+    ]
+  }), sitemap(), mdx(), react(), tailwind()]
 });
