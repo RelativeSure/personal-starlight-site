6b44166f8614727811ef090826846a4ab3c7091f
RelativeSure <mail@rasmusj.dk>
2025-03-01 12:35:01 +0100

    Cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,61 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightImageZoom from "starlight-image-zoom";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import robotsTxt from "astro-robots-txt";
-import { ion } from "starlight-ion-theme";
 import icon from "astro-icon";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rasmusj.dk",
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    routes: {
+      extend: {
+        include: [{ pattern: '/static' }], // Route a prerended page to the SSR function for on-demand rendering
+        include: [{ pattern: '/dist' }], // Route a prerended page to the SSR function for on-demand rendering
+        exclude: [{ pattern: '/pagefind/*' }], // Use Starlight's pagefind search, which is generated statically at build time
+      }
+    },
+  }),
+  prerender: false,
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      external: ['node:path', 'node:url'],
+    },
   },
   redirects: {
       "/linkedin": "https://www.linkedin.com/in/rasmusbroeggerjoergensen/"
   },
   integrations: [
     starlight({
-      plugins: [
-        starlightImageZoom(),
-        ion()
-      ],
       title: "Rasmus Brøgger Jørgensen",
       tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
       lastUpdated: true,
       logo: {
         src: './src/assets/favicon.svg',
       },
+      editLink: {
+        baseUrl: 'https://github.com/RelativeSure/personal-starlight-site/edit/main/',
+      },
       expressiveCode: {
         themes: ['material-theme-ocean', 'material-theme-lighter'],
       },
       customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css',
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+        './src/styles/global.css',
         // Fontsource files for to regular and semi-bold font weights.
         "@fontsource/fira-mono/400.css",
         "@fontsource/fira-mono/500.css",
         "@fontsource/fira-mono/700.css",
-        // Relative path to your custom CSS file
-        './src/styles/custom.css',
-        './src/styles/globals.css',
       ],
       social: {
         github: "https://github.com/RelativeSure",
@@ -62,24 +77,12 @@ export default defineConfig({
             directory: "goodstuff",
           },
         },
-        // {
-        //   label: "Kubernetes",
-        //   autogenerate: {
-        //     directory: "kubernetes",
-        //   },
-        // },
         {
           label: "Linux",
           autogenerate: {
             directory: "linux",
           },
         },
-        // {
-        //   label: "Windows",
-        //   autogenerate: {
-        //     directory: "windows",
-        //   },
-        // },
         {
           label: "Projects",
           autogenerate: {
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,23 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+// import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+// import { glob } from 'astro/loaders';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
-const pages = defineCollection({
-  loader: glob({ pattern: "**/*.astro", base: "./src/pages" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-  }),
-});
+
+// For pages
+// const pages = defineCollection({
+//   loader: glob({ pattern: "**/*.astro", base: "./src/pages" }),
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//   }),
+// });
 
 const docs = defineCollection({
   loader: docsLoader(),
   schema: docsSchema(),
 });
 
-export const collections = { pages, docs };
+// export const collections = { pages, docs };
+export const collections = { docs };
