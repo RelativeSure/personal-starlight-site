6bf65afd39437468df27a6fa328b5f9c378063de
KubeArchitectBot <139585163+KubeArchitectBot@users.noreply.github.com>
2025-04-16 21:29:58 +0200

    fix(deps): update dependency @astrojs/starlight to v0.34.0 (#756)
    * fix(deps): update dependency @astrojs/starlight to v0.34.0
    
    * Update tailwind.css
    
    * Update astro.config.mjs
    
    ---------
    
    Co-authored-by: Rasmus <67839982+RelativeSure@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,12 +36,12 @@ export default defineConfig({
         "@fontsource/fira-mono/500.css",
         "@fontsource/fira-mono/700.css",
       ],
-      social: {
-        github: "https://github.com/RelativeSure",
-        mastodon: "https://infosec.exchange/@relativesure",
-        "x.com": "https://x.com/RelativeSure",
-        linkedin: "https://www.linkedin.com/in/rasmusbroeggerjoergensen/",
-      },
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/RelativeSure" },
+        { icon: "mastodon", label: "Mastodon", href: "https://infosec.exchange/@relativesure" },
+        { icon: "x.com", label: "X", href: "https://x.com/RelativeSure" },
+        { icon: "linkedin", label: "LinkedIn", href: "https://www.linkedin.com/in/rasmusbroeggerjoergensen/" },
+      ],
       sidebar: [
         {
           label: "Bookmarks",
